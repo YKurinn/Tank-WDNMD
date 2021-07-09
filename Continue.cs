5f4e96358de1616f7e5d3c28049901724e034046
Y Kurin <80440840+YKurinn@users.noreply.github.com>
2021-07-09 17:43:03 +0800

    Add files via upload

---
+++ Continue.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class Continue : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public GameObject pauseCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+
+    }
+
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Time.timeScale = 1;
+        pauseCanvas.SetActive(false);
+    }
+}
